9628033314da07f8237f6586a6dec6447ddc8576
AbhayThagle01 <97532074+AbhayThagle01@users.noreply.github.com>
2023-04-27 00:02:57 +0530

    Update Footer.jsx

--- Helper/frontnnd/src/components/Footer.jsx
+++ Helper/frontnnd/src/components/Footer.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
+
+
 const Footer = () => {
+   
+        const styles = {
+          backgroundColor: '#333',
+          color: '#fff',
+          padding: '10px',
+          textAlign: 'center'
+        }
     return (
-        <div>
-            
-        </div>
+        <div style={styles}>
+      <p>Creator : <b>Abhay Thagle, Anirudh Yadav, Anshuman Tamrakar</b> </p>
+      {/* <p>It has a dark background and white text.</p>
+      <p>The text is centered and there is some padding around the content.</p> */}
+    </div>
     )
 }
 
 export default Footer
+
